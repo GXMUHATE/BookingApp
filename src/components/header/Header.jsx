710d43f316adbdc7b00ec7371777999e9eb91f0e
gmuhate <gracane.muhate@nbf-web.com>
2022-08-31 15:23:25 +0200

    Creating Booking App UI - Adding ListSearch component details

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,8 +6,10 @@ import { DateRange } from "react-date-range";
 import 'react-date-range/dist/styles.css'; // main css file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import {format} from "date-fns"
+import { useNavigate } from "react-router-dom";
 
 const Header = ({type}) => {
+    const [destination, setDestination] = useState("")
     const [openDate, setOpenDate] = useState(false)
     const [date, setDate] = useState([
         {
@@ -16,12 +18,20 @@ const Header = ({type}) => {
           key: 'selection'
         }
       ]);
+      const navigate = useNavigate()
+
       const [openOptions, setOpenOptions] = useState(false)
       const [options,setOptions] = useState({
         adult:1,
         children:0,
         room:1,
       });
+
+      
+
+      const handleSearch = () =>{
+        navigate("/hotels", {state: {destination,date,options}})
+      }
 
       const handleOption = (name, operation) => {
         setOptions((prev) => {
@@ -70,7 +80,8 @@ const Header = ({type}) => {
                             <input 
                                 type="text" 
                                 placeholder="Where are you going?" 
-                                className="headerSearchInput" 
+                                className="headerSearchInput"
+                                onChange={e=>setDestination(e.target.value)} 
                             />
                         </div>
                         <div className="headerSearchItem">
@@ -83,6 +94,7 @@ const Header = ({type}) => {
                                 moveRangeOnFirstSelection={false}
                                 ranges={date}
                                 className="date"
+                                minDate={new Date()}
                             />}
                         </div>
                         <div className="headerSearchItem">
@@ -123,7 +135,7 @@ const Header = ({type}) => {
                             </div>}
                         </div>
                         <div className="headerSearchItem">
-                            <button className="headerBtn">Search</button>
+                            <button className="headerBtn" onClick={handleSearch}>Search</button>
                         </div>
                     </div> 
                 </>}
