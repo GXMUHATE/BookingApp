72c25f374c3826b75b5667976b9e0d493c487027
gmuhate <gracane.muhate@nbf-web.com>
2022-09-16 12:54:53 +0200

    Implementing Fetch Data using Custom Hook

--- client/src/components/featured/featured.jsx
+++ client/src/components/featured/featured.jsx
@@ -1,29 +1,39 @@
+import useFetch from "../hooks/useFetch"
 import "./featured.css"
 
 const Featured = () => {
+
+    const { data, loading, error } = useFetch("hotels/countByCity?cities=Maputo,Tokyo,Cape Town")
+
+
     return (
         <div className="featured">
-            <div className="featuredItem">
-                <img src={require('../../images/woman1.png')} alt="paradise" className="featuredImg" />
-                <div className="featuredTitles">
-                    <h1>Manchester</h1>
-                    <h2>320 properties</h2>
-                </div>
-            </div>
-            <div className="featuredItem">
-                <img src={require('../../images/thailand.png')} alt="paradise" className="featuredImg" />
-                <div className="featuredTitles">
-                    <h1>Thailand</h1>
-                    <h2>120 properties</h2>
-                </div>
-            </div>
-            <div className="featuredItem">
-                <img src={require('../../images/paradise.png')} alt="paradise" className="featuredImg" />
-                <div className="featuredTitles">
-                    <h1>Maldives</h1>
-                    <h2>80 properties</h2>
-                </div>
-            </div>
+
+            {loading ? (
+                "Loading please wait"
+            ) : (
+                <>
+                    <div className="featuredItem">
+                        <img src={require('../../images/woman1.png')} alt="paradise" className="featuredImg" />
+                        <div className="featuredTitles">
+                            <h1>Maputo</h1>
+                            <h2>{data[0]} properties</h2>
+                        </div>
+                    </div>
+                    <div className="featuredItem">
+                        <img src={require('../../images/thailand.png')} alt="paradise" className="featuredImg" />
+                        <div className="featuredTitles">
+                            <h1>Tokyo</h1>
+                            <h2>{data[1]} properties</h2>
+                        </div>
+                    </div>
+                    <div className="featuredItem">
+                        <img src={require('../../images/paradise.png')} alt="paradise" className="featuredImg" />
+                        <div className="featuredTitles">
+                            <h1>Cape Town</h1>
+                            <h2>{data[2]} properties</h2>
+                        </div>
+                    </div></>)}
         </div>
     )
 }
