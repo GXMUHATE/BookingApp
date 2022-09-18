d6534fbc3900031c5331141423995704ff6febed
gmuhate <gracane.muhate@nbf-web.com>
2022-09-18 21:07:20 +0200

    Fetch Hotel using URL location

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -6,32 +6,37 @@ import MailList from "../../components/mailList/MailList"
 import "./hotel.css"
 import Footer from "../../components/footer/Footer"
 import { useState } from "react"
+import useFetch from "../../components/hooks/useFetch"
+import { useLocation } from "react-router-dom"
 
 const Hotel = () => {
-
+    const location = useLocation()
+    const id= location.pathname.split("/")[2]
     const [slideNumber, setSlideNumber] = useState(0);
     const [open, setOpen] = useState(false);
 
-    const photos = [
-        {
-            src:require('../../images/f3.jpeg')
-        },
-        {
-            src:require('../../images/f1.jpeg')
-        },
-        {
-            src:require('../../images/f2.jpeg')
-        },
-        {
-            src:require('../../images/f4.jpeg')
-        },
-        {
-            src:require('../../images/f5.jpeg')
-        },
-        {
-            src:require('../../images/f4.jpeg')
-        }
-    ];
+    const { data, loading, error } = useFetch(`/hotels/find/${id}`)
+
+    // const photos = [
+    //     {
+    //         src:require('../../images/f3.jpeg')
+    //     },
+    //     {
+    //         src:require('../../images/f1.jpeg')
+    //     },
+    //     {
+    //         src:require('../../images/f2.jpeg')
+    //     },
+    //     {
+    //         src:require('../../images/f4.jpeg')
+    //     },
+    //     {
+    //         src:require('../../images/f5.jpeg')
+    //     },
+    //     {
+    //         src:require('../../images/f4.jpeg')
+    //     }
+    // ];
 
     const handleOpen = (i) => {
         setSlideNumber(i);
@@ -54,47 +59,43 @@ const Hotel = () => {
         <div>
             <Navbar/>
             <Header type="list"/>
-            {open && <div className="slider">
+            {loading ? (
+                "Loading"
+                ) : (
+                     <div className="hotelContainer">
+            {open && (<div className="slider">
                     <FontAwesomeIcon icon={faCircleXmark} className="close" onClick={()=>setOpen(false)}/>
                     <FontAwesomeIcon icon={faCircleArrowLeft} className="arrow" onClick={()=>handleMove("l")}/>
                     <div className="sliderWrapper">
-                        <img src={photos[slideNumber].src} alt="" className="sliderImg" />
+                        <img src={data.photos[slideNumber]} alt="" className="sliderImg" />
                     </div>
                     <FontAwesomeIcon icon={faCircleArrowRight} className="arrow" onClick={()=>handleMove("r")}/>
-                </div>}
-            <div className="hotelContainer">
+                </div>)}
                 <div className="hotelWrapper">
                     <button className="bookNow">Reserve or Book Now!</button>
-                    <h1 className="hotelTitle">Grand Hotel</h1>
+                    <h1 className="hotelTitle">{data.name}</h1>
                     <div className="hotelAddress">
                         <FontAwesomeIcon icon={faLocationDot}/>
-                        <span>Elton St 136 Maputo</span>
+                        <span>{data.address}</span>
                     </div>
                     <span className="hotelDistance">
-                        Excellent location - 500m from center
+                        Excellent location - {data.distance}m from center
                     </span>
                     <span className="hotelPriceHighlight">
-                        Book a stay over $123 at this property and get a free airport taxi
+                        Book a stay over ${data.cheapestPrice} at this property and get a free airport taxi
                     </span>
                     <div className="hotelImages">
-                        {photos.map((photo, i)=>(
+                        {data.photos?.map((photo, i)=>(
                             <div className="hotelImgWrapper">
-                                <img onClick={() => handleOpen(i)} src={photo.src} alt="" className="hotelImg" />
+                                <img onClick={() => handleOpen(i)} src={photo} alt="" className="hotelImg" />
                             </div>
                         ))}
                     </div>
                     <div className="hotelDetails">
                         <div className="hotelDetailsTexts">
-                            <h1 className="hotelTitle">Stay in the heart of Krakow</h1>
+                            <h1 className="hotelTitle">{data.title}</h1>
                             <p className="hotelDesc">
-                            In the Salford district of Manchester, close to Opera House Manchester, 
-                            Atelier Chapel Apartments features a garden, free WiFi and a washing machine. This property offers access to a pool table and darts.This apartment is equipped with 2 bedrooms, a kitchen with a dishwasher and a microwave, a flat-screen TV, a seating area and 2 bathrooms equipped with a shower. Towels and bed linen are provided.
-                            The apartment provides a terrace.
-                            Popular points of interest near Atelier Chapel Apartments include John Rylands Library, Royal Exchange Theatre and Albert Square. The nearest airport is Manchester Airport, 13 km from the accommodation.
-                            This is our guests' favourite part of Manchester, according to independent reviews.
-                            Atelier Chapel Apartments has been welcoming Booking.com guests since 18 Aug 2022.
-                            Distance in property description is calculated using © OpenStreetMap
-
+                            {data.desc}
                             </p>
                         </div>
                         <div className="hotelDetailsPrice">
@@ -111,8 +112,10 @@ const Hotel = () => {
                     </div>
                 </div>
             </div>
+            )}
             <MailList/>
             <Footer/>
+            
         </div>
     )
 }
