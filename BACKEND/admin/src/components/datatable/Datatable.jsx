7756a29a53c5420ef6babe3def264caa693d343e
gmuhate <gracane.muhate@nbf-web.com>
2022-10-10 17:17:06 +0200

    configuring user delete function

--- BACKEND/admin/src/components/datatable/Datatable.jsx
+++ BACKEND/admin/src/components/datatable/Datatable.jsx
@@ -1,15 +1,27 @@
 import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { userColumns, userRows } from "../../datatablesource";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useState } from "react";
 import useFetch from "../../hooks/useFetch"
+import { useEffect } from "react";
+import axios from "axios";
 
 const Datatable = () => {
-  const {data,loading,error} = useFetch("/users")
+  const location = useLocation();
+  const path = location.pathname.split("/")[1]
+  const [list, setList] = useState();
+  const {data,loading,error} = useFetch(`/${path}`)
 
-  const handleDelete = (id) => {
-    // setData(data.filter((item) => item.id !== id));
+  useEffect(()=> {
+    setList(data)
+  }, [data])
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`/${path}/${id}`)
+      setList(list.filter((item) => item._id !== id));
+    } catch (err) {}
   };
 
   const actionColumn = [
@@ -44,7 +56,7 @@ const Datatable = () => {
       </div>
       <DataGrid
         className="datagrid"
-        rows={data}
+        rows={list}
         columns={userColumns.concat(actionColumn)}
         pageSize={9}
         rowsPerPageOptions={[9]}
